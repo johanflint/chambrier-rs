7a96c5c8815a73c34d57e8add5895ab6455f0113
Johan Flint <johan.flint@gmail.com>
2023-12-14 11:17:49 +0100

    Connect to Hue to retrieve the list of devices

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::header::{HeaderMap, HeaderValue};
+use reqwest::Client;
+use serde_json::Value;
+use std::env;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = http_client()?;
+
+    println!("Retrieving devices from Philips Hue...");
+    let response = client
+        .get(format!(
+            "https://{}/clip/v2/resource",
+            env::var("HUE_ENDPOINT")?
+        ))
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+    eprintln!("response = {:#?}", response);
+    Ok(())
+}
+
+fn http_client() -> Result<Client, reqwest::Error> {
+    let hue_application_key = env::var("HUE_APP_KEY").unwrap();
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        "hue-application-key",
+        HeaderValue::from_str(&hue_application_key).unwrap(),
+    );
+    Client::builder()
+        .gzip(true)
+        .danger_accept_invalid_certs(true)
+        .default_headers(headers)
+        .build()
 }
